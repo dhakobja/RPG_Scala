d300f0de60813e3d40ca74953af21c73b23c8413
dhakobja <dhakobja@web.de>
2024-12-16 21:17:21 +0100

    Added basic structure of the project

--- src/main/scala/entry_point.scala
+++ src/main/scala/entry_point.scala
@@ -1,5 +1,17 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+import Game_Entities.{
+    Player
+}
 
-def msg = "I was compiled by Scala 3. :)"
+import scala.io.StdIn.readLine
+
+@main def game(): Unit = {
+    print("Enter your Name: ")
+    val playerName: String = readLine()
+    var mainPlayer: Player = Player(playerName)
+    
+    while (true) {
+      println("You wake up in a dark, damp cave. The air smells of mildew, and the faint sound of dripping " +
+        "water echoes around you. There's a faint glimmer in the corner.")
+    }
+}
+
